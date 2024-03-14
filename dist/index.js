e0589ccba573f6ea91fb94c58ed878427fc0ce88
Caleb Owatah <owatahcaleb@gmail.com>
2024-03-14 14:09:09 +0100

    arrays and object

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,15 @@
 "use strict";
 let myName = "Caleb";
-console.log(myName);
+// ARRAYS
+const fruits = ["apple", "mango", "pear"];
+const numbers = [2, 3, 4, 5, 7, 898, 87];
+// numbers.push(0)
+// OBJECTS
+const person = {
+    name: "caleb",
+    age: 22,
+    score: 100,
+    isLoggedIn: false,
+};
+person.isLoggedIn = true;
+console.log(person);
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,24 @@
-let myName = "Caleb";
+let myName: string = "Caleb";
 
-console.log(myName);
+// ARRAYS
+const fruits: string[] = ["apple", "mango", "pear"];
+const numbers = [2, 3, 4, 5, 7, 898, 87];
+
+// numbers.push(0)
+
+// OBJECTS
+const person: {
+  name: string;
+  age: number;
+  score: number;
+  isLoggedIn: boolean;
+} = {
+  name: "caleb",
+  age: 22,
+  score: 100,
+  isLoggedIn: false,
+};
+
+person.isLoggedIn = true;
+
+console.log(person);
